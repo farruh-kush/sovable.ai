67fe41c3ddba42f5226f3732515c53a3956023e8
k-farruh <k.farruh@bk.ru>
2026-08-16 15:51:16 +0800

    feat: add Solvable favicon branding

--- web/dashboard/pages/_app.tsx
+++ web/dashboard/pages/_app.tsx
@@ -1,4 +1,19 @@
+import Head from 'next/head'
 import React from 'react'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-export default function App({ Component, pageProps }: AppProps) { return <Component {...pageProps} /> }
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Solvable AI</title>
+        <meta name="theme-color" content="#12233f" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon-180.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
